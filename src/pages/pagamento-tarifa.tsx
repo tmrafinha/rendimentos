866bd5993ced44e91ce643a5f4a17276525c05d2
unknown <tmrafinha4@gmail.com>
2025-02-06 16:32:33 -0300

    back to 29 90

--- src/pages/pagamento-tarifa.tsx
+++ src/pages/pagamento-tarifa.tsx
@@ -146,7 +146,7 @@ export function PagamentoTarifa() {
                         <p className="text-2xl mb-2 text-primary font-medium">Taxa de <br /> administração Caixa:</p>
                     </div>
                     {/* <p className="text-2xl mb-2 text-zinc-400">DE: R$47,89</p> */}
-                    <p className="text-4xl font-semibold text-primary">R$19,90</p>
+                    <p className="text-4xl font-semibold text-primary">R$29,90</p>
                 </div>
 
                 {/* Sessão com imagem do Pix e mensagem de justificativa */}
